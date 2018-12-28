a9abfe44acea4121e196a6e341f1233bf83b3b0e
Art <artur.malvarez92@mail.com>
2018-12-28 12:00:18 +0000

    Dices App

---
+++ Dices/Dices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dices
+//
+//  Created by apple on 28/12/2018.
+//  Copyright © 2018 TECNOLOGIA EN SERVICIOS BIOMEDICOS SAPI DE CV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
